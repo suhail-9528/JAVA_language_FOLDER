0daaa1a53ef2c99550468d470682f6a9567421e7
Your Name <you@example.com>
2024-02-27 15:47:32 +0530

    new add java folder

---
+++ main.java
@@ -0,0 +1,12 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        var ss=23;
+        var s=64.8;
+        var d=s+ss;
+        System.out.print(d);
+    }
+}
