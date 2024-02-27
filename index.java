2e704b242f293735f0205d38b61624289aaad943
Your Name <you@example.com>
2024-02-27 18:02:27 +0530

    d

--- index.java
+++ index.java
@@ -5,5 +5,20 @@ public class suhail{
     System.out.println("hello world");
   }
 
+public static void tasneem1(String[] args){
+
+var zero_java=()=>{
+  var ss=7;
+ for(var i=0; i<10; i++){
+  System.out.println(i);
+  if(ss ==i){
+    System.out.println("you are stop");
+  }
+ }
+
+}
+
+}
+
 
 }
